265f6116fcef56b932d7606698cc26d828c3400a
sameh-dh <samehdh44@gmail.com>
2023-08-16 15:07:34 +0100

    navbar updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,11 +12,11 @@ const Navbar = () => {
       <ul className={sidebar ? " navbar-items-container active" : "navbar-items-container "}>
       <img className={sidebar ?'navbar-logo active' :'navbar-logo' } src={logo}></img>
         <li className="navbar-items"><a className={activated[0]} href='home' onClick={()=>setActivated(["active","","","",""])}>
-        <i className="uil uil-estate nav__icon"></i>Who we are ?</a></li>
+        <i className="uil  uil-question-circle nav__icon"></i>Who we are ?</a></li>
         <li className="navbar-items"><a className={activated[1]} href='#services' onClick={()=>setActivated(["","active","","","","",""])}> <i className="uil uil-file-alt nav__icon"></i> services</a></li>
-        <li className="navbar-items"><a className={activated[2]} href='#work' onClick={()=>setActivated(["","","active","","","",""])}> <i className="uil uil-scenery nav__icon"></i> Blogs</a></li>
-        <li className="navbar-items"><a className={activated[3]} href='#work' onClick={()=>setActivated(["","","","active","","",""])}> <i className="uil uil-scenery nav__icon"></i>Products</a></li>
-        <li className="navbar-items"><a className={activated[4]} href="#contact" onClick={()=>setActivated(["","","","","active",""])}><i className="uil uil-user nav__icon"></i> Contact</a></li>
+        <li className="navbar-items"><a className={activated[2]} href='#work' onClick={()=>setActivated(["","","active","","","",""])}> <i className="uil uil-document-layout-left nav__icon"></i> Blogs</a></li>
+        <li className="navbar-items"><a className={activated[3]} href='#work' onClick={()=>setActivated(["","","","active","","",""])}> <i className="uil uil-box nav__icon"></i>Products</a></li>
+        <li className="navbar-items"><a className={activated[4]} href="#contact" onClick={()=>setActivated(["","","","","active",""])}><i className="uil uil-phone-volume nav__icon"></i> Contact</a></li>
     
       
     
